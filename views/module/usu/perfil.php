2831d6090657d64ac4e3e8a81aecfafcac1026a0
sara0106 <smunoz476@misena.edu.co>
2017-04-21 16:59:52 -0500

    Cambios
    ss

---
+++ views/module/usu/perfil.php
@@ -0,0 +1,138 @@
+<div class="container">
+    <div class="row user-menu-container square">
+        <div class="col-md-7 user-details">
+            <div class="row coralbg white">
+                <div class="col-md-6 no-pad">
+                    <div class="user-pad">
+                        <h3>Bienvenido, Jhony</h3>
+                        <h4 class="white"><i class="fa fa-check-circle-o"></i> Itagüí, ANT</h4>
+                        <h4 class="white"><i class="fa fa-twitter"></i> alexandercos14@gmail.com</h4>
+                        <button type="button" class="btn btn-labeled btn-info" href="#">
+                            <span class="btn-label"><i class="fa fa-pencil"></i></span>Editar</button>
+                    </div>
+                </div>
+                <div class="col-md-6 no-pad">
+                    <div class="user-image">
+                        <img src="https://farm7.staticflickr.com/6163/6195546981_200e87ddaf_b.jpg" class="img-responsive thumbnail">
+                    </div>
+                </div>
+            </div>
+            <div class="row overview">
+                <div class="col-md-4 user-pad text-center">
+                    <h3>FOLLOWERS</h3>
+                    <h4>2,784</h4>
+                </div>
+                <div class="col-md-4 user-pad text-center">
+                    <h3>FOLLOWING</h3>
+                    <h4>456</h4>
+                </div>
+                <div class="col-md-4 user-pad text-center">
+                    <h3>APPRECIATIONS</h3>
+                    <h4>4,901</h4>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-1 user-menu-btns">
+            <div class="btn-group-vertical square" id="responsive">
+                <a href="#" class="btn btn-block btn-default active">
+                  <i class="fa fa-bell-o fa-3x"></i>
+                </a>
+                <a href="#" class="btn btn-default">
+                  <i class="fa fa-envelope-o fa-3x"></i>
+                </a>
+                <a href="#" class="btn btn-default">
+                  <i class="fa fa-laptop fa-3x"></i>
+                </a>
+                <a href="#" class="btn btn-default">
+                  <i class="fa fa-cloud-upload fa-3x"></i>
+                </a>
+            </div>
+        </div>
+        <div class="col-md-4 user-menu user-pad">
+            <div class="user-menu-content active">
+                <h3>
+                    Actividad Reciente
+                </h3>
+                <ul class="user-menu-list">
+                    <li>
+                        <h4><i class="fa fa-user coral"></i> Buscaste Hospital: San Vicente de Paul</h4>
+                    </li>
+                    <li>
+                        <h4><i class="fa fa-heart-o coral"></i> Recibiste un correo de: Dr.Mandingo</h4>
+                    </li>
+                    <li>
+                        <h4><i class="fa fa-paper-plane-o coral"></i> Has sido aceptado como donante.</h4>
+                    </li>
+                    <li>
+                        <button type="button" class="btn btn-labeled btn-success" href="#">
+                            <span class="btn-label"><i class="fa fa-bell-o"></i></span>Ver toda la actividad</button>
+                    </li>
+                </ul>
+            </div>
+            <div class="user-menu-content">
+                <h3>
+                    Tu Inbox
+                </h3>
+                <ul class="user-menu-list">
+                    <li>
+                        <h4>De: Brayan <small class="coral"><strong>NEW</strong> <i class="fa fa-clock-o"></i> 7:42 A.M.</small></h4>
+                    </li>
+                    <li>
+                        <h4>De: Yurledys <small class="coral"><i class="fa fa-clock-o"></i> 10:23 A.M.</small></h4>
+                    </li>
+                    <li>
+                        <h4>De: Daniela Solomillloslks <small class="coral"><i class="fa fa-clock-o"></i> 11:19 A.M.</small></h4>
+                    </li>
+                    <li>
+                        <button type="button" class="btn btn-labeled btn-danger" href="#">
+                            <span class="btn-label"><i class="fa fa-envelope-o"></i></span>Ver todos los mensajes</button>
+                    </li>
+                </ul>
+            </div>
+            <div class="user-menu-content">
+                <h3>
+                    Mi Información
+                </h3>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="view">
+                            <div class="caption">
+                                <p>Editar Información</p>
+                                <a href="" rel="tooltip" title="Appreciate"><span class="fa fa-pencil fa-2x"></span></a>
+                            </div>
+                            <img src="http://wallpapercave.com/wp/zeFfMoc.jpg" class="img-responsive">
+                        </div>
+                        <div class="info">
+                            <p class="small" style="text-overflow: ellipsis">Mi Información Física</p>
+                            <p class="small coral text-right"><i class="fa fa-clock-o"></i> Ultima Actualización Hoy | 10:42 A.M.</small>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="view">
+                            <div class="caption">
+                                <p>Editar Información</p>
+                                <a href="" rel="tooltip" title="Appreciate"><span class="fa fa-pencil fa-2x"></span></a>
+                            </div>
+                            <img src="http://wallpapercave.com/wp/zeFfMoc.jpg" class="img-responsive">
+                        </div>
+                        <div class="info">
+                            <p class="small" style="text-overflow: ellipsis">Mi Información Médica</p>
+                            <p class="small coral text-right"><i class="fa fa-clock-o"></i>Ultima Actualización Ayer | 07:08 P.M.</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="user-menu-content">
+                <h2 class="text-center">
+                    Comenzar Ahora
+                </h2>
+                <center><i class="fa fa-cloud-upload fa-4x"></i></center>
+                <div class="share-links">
+                    <center><button type="button" class="btn btn-lg btn-labeled btn-success" href="#" style="margin-bottom: 15px;">
+                            <span class="btn-label"><i class="fa fa-bell-o"></i></span>COMENZAR PROCESO DE BUSQUEDA
+                    </button></center>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
