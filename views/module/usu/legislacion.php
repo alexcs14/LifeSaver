fd2594983b5e8f7c2b7bd29edd76498f8010311b
Estebanvasquez07 <evasquez70@misena.edu.co>
2017-05-25 15:39:29 -0500

    Ultimos Cambios
    Terminación de vistas

--- views/module/usu/legislacion.php
+++ views/module/usu/legislacion.php
@@ -2,14 +2,14 @@
 <div class="content-leg">
 	<div class="blog" id="blog">
 		<div class="container">
-		<h3 class="tittle">Latest News</h3>
-		<p class="heading-btm-w3ls">Suspendisse fringilla pellentesque risus sit amet bibendum.</p>
+		<h3 class="tittle">LEGISLACIÓN</h3>
+		<p class="heading-btm-w3ls">Las bases que fundamentan un servicio correcto para cada individuo.</p>
 			<div class="blog-grid-w3-agileits">
 				<div class=" col-md-7 blog-img1-agileits-w3layouts">
 				<a  href="#" data-toggle="modal" data-target="#myModal2"></a>
 				</div>
 				<div class=" col-md-5 blog-info-w3layouts">
-					<h6> <a  href="#" data-toggle="modal" data-target="#myModal2">FURTHER DEVELOPMENT IN AGRICULTURE GROWTH</a></h6>
+					<h6> <a  href="#" data-toggle="modal" data-target="#myModal2">PARTE DE LEGISLACIÓN 1</a></h6>
 					<div class="inner-info">
 						<a href="#" class="blog-text-w3ls bar">Lorem ipsum.</a><span>16/11/2016</span>
 						<p class="para-w3-agileits">Aenean pulvinar diam vel felis volutpat dictum. Aenean ac tellus suscipit sapien scelerisque tempus non mollis massa.</p>
@@ -21,7 +21,7 @@
 			</div>
 			<div class="blog-grid-w3-agileits">
 				<div class=" col-md-5 blog-info-w3layouts blog-mid">
-					<h6><a  href="#" data-toggle="modal" data-target="#myModal3">KEEPING ON ADVANCING THE AGRICULTURE INDUSTRY</a></h6>
+					<h6><a  href="#" data-toggle="modal" data-target="#myModal3">PARTE DE LEGISLACIÓN 2</a></h6>
 					<div class="inner-info-mid">
 						<a href="#" class="blog-text-w3ls mid-bar">Lorem ipsum.</a><span>12/10/2016</span>
 						<p class="para-w3-agileits">Aenean pulvinar diam vel felis volutpat dictum. Aenean ac tellus suscipit sapien scelerisque tempus non mollis massa.</p>
@@ -39,7 +39,7 @@
 					<a  href="#" data-toggle="modal" data-target="#myModal4"></a>
 				</div>
 				<div class=" col-md-5 blog-info-w3layouts">
-					<h6><a  href="#" data-toggle="modal" data-target="#myModal4"> OUR FIRST STEPS ON THE MARKET AMONG AGRICULTURAL COMPANIES</a></h6>
+					<h6><a  href="#" data-toggle="modal" data-target="#myModal4"> PARTE DE LEGISLACIÓn 3</a></h6>
 					<div class="inner-info">
 						<a href="#" class="blog-text-w3ls bar">Lorem ipsum.</a><span>06/8/2016</span>
 						<p class="para-w3-agileits">Aenean pulvinar diam vel felis volutpat dictum. Aenean ac tellus suscipit sapien scelerisque tempus non mollis massa.</p>
@@ -60,9 +60,9 @@
 								<div class="modal-content">
 									<div class="modal-header">
 										<button type="button" class="close" data-dismiss="modal">&times;</button>
-										<h4>In Harvest</h4>
+										<h4>LEGISLACIÓN</h4>
 										<img src="views/assets/img/b1.jpg" alt=" " class="img-responsive">
-										<h5>FURTHER DEVELOPMENT IN AGRICULTURE GROWTH</h5>
+										<h5>PARTE DE LEGISLACIÓN 1</h5>
 										<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting.</p>
 									</div>
 								</div>
@@ -76,9 +76,9 @@
 								<div class="modal-content">
 									<div class="modal-header">
 										<button type="button" class="close" data-dismiss="modal">&times;</button>
-										<h4>In Harvest</h4>
+										<h4>LEGISLACIÓN</h4>
 										<img src="views/assets/img/b2.jpg" alt=" " class="img-responsive">
-										<h5>KEEPING ON ADVANCING THE AGRICULTURE INDUSTRY</h5>
+										<h5>PARTE DE LEGISLACIÓN 2</h5>
 										<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting.</p>
 									</div>
 								</div>
@@ -92,9 +92,9 @@
 								<div class="modal-content">
 									<div class="modal-header">
 										<button type="button" class="close" data-dismiss="modal">&times;</button>
-										<h4>In Harvest</h4>
+										<h4>LEGISLACIÓN</h4>
 										<img src="views/assets/img/b3.jpg" alt=" " class="img-responsive">
-										<h5>OUR FIRST STEPS ON THE MARKET AMONG AGRICULTURAL COMPANIES</h5>
+										<h5>PARTE DE LEGISLACIÓN 3</h5>
 										<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting.</p>
 									</div>
 								</div>
